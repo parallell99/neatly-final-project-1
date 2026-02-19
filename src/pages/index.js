ddc8e06354d2e407c560ba35c5c9b1351e5b54e4
parallell99 <jr.projectx@gmail.com>
2026-02-19 12:14:02 +0700

    feat: add HeroSlide component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Button from "@/components/ui/buttons/buttons";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
+import HeroSlide from "@/components/layout/heroSlide";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,7 +19,7 @@ export default function Home() {
       className={`${geistSans.className} ${geistMono.className} flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans w-full`}
     >
       <Navbar />
-   
+      <HeroSlide />
       <Footer />
     </div>
   );
