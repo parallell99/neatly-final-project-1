b0576d38710fecb00b850045d025a3abf5115700
MOGunyaluck <gunyaluck0619@gmail.com>
2026-03-19 11:12:18 +0700

    refactor(payment): adjust font sizes for consistency and update iframe dimensions for better layout on payment and checkout pages

--- src/pages/payment-method/index.js
+++ src/pages/payment-method/index.js
@@ -256,8 +256,8 @@ export default function PaymentMethodPage() {
                       locale: "en",
                       appearance: {
                         variables: {
-                          fontSizeBase: isLg ? "16px" : "12px",
-                          fontSizeSm: isLg ? "16px" : "12px",
+                          fontSizeBase: isLg ? "14px" : "12px",
+                          fontSizeSm: isLg ? "14px" : "12px",
                         },
                       },
                     }}
